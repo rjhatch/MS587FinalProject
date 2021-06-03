b0d39e4688040ef2416c7f3c7f9302577e45a219
richie hatch <rjh378@gmail.com>
2021-06-03 14:50:58 -0600

    Redirect to login in not authorized. Started work on a schedule display.

---
+++ Components/RedirectToLogin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MS587FinalProject.Components
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo("Identity/Account/Login");
+        }
+    }
+}
